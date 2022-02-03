433442dfa4d4e8da37c3eebed03fc75e625479ec
JakeLDev <jakelyell@iinet.net.au>
2022-02-03 19:45:14 +0800

    Project init

---
+++ webcamtest.py
@@ -0,0 +1,68 @@
+import cv2
+import numpy as np
+# import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import pandas as pd
+import numpy as np
+from keras.models import Sequential
+# from keras.layers import *
+# from tensorflow.keras.preprocessing.image import ImageDataGenerator
+# from keras.applications import DenseNet121
+# import os
+# import cv2
+# from numpy import asarray
+# from PIL import Image
+# from functools import partial
+# from keras.applications import imagenet_utils
+load_model = keras.models.load_model('densenet121_detection_model.h5')
+
+labels_dict={1:'without_mask',0:'with_mask'}
+color_dict={0:(0,0,255),1:(0,255,0)}
+
+size = 4
+cv2.namedWindow("COVID Mask Detection Video Feed")
+webcam = cv2.VideoCapture(0) 
+
+classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+while True:
+    rval, im = webcam.read()
+    im=cv2.flip(im,1,1)
+    
+    mini = cv2.resize(im, (im.shape[1] // size, im.shape[0] // size))
+ 
+    faces = classifier.detectMultiScale(mini)
+
+    for f in faces:
+        (x, y, w, h) = [v * size for v in f] 
+        face_img = im[y:y+h, x:x+w]
+        resized=cv2.resize(face_img,(224,224))
+        normalized=resized/255.0
+        reshaped=np.reshape(normalized,(1,224,224,3))
+        reshaped = np.vstack([reshaped])
+        result=load_model.predict(reshaped)
+        print(result)
+        if result[0][0] > result[0][1]:
+            percent = round(result[0][0]*100,2)
+        else:
+            percent = round(result[0][1]*100,2)
+        
+        label=np.argmax(result,axis=1)[0]
+        cv2.rectangle(im,(x,y),(x+w,y+h),color_dict[label],2)
+        cv2.rectangle(im,(x,y-40),(x+w,y),color_dict[label],-1)
+        cv2.putText(im, labels_dict[label] + " " + str(percent) + "%", (x, y-10),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
+        
+    if im is not None:   
+        cv2.imshow('COVID Mask Detection Video Feed', im)
+    key = cv2.waitKey(10)
+    
+    # Exit
+    if key == 27: #The Esc key
+        break
+        
+# Stop video
+webcam.release()
+
+# Close all windows
+cv2.destroyAllWindows()
